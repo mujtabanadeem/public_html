5954047513d6539a43c83c86e94ad6d154fccd39
mujtabanadeem <mujtaba.nadeem4@gmail.com>
2023-01-14 20:48:13 +0000

    test

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,12 +1,3 @@
 
-<<<<<<< HEAD
 
   document.body.style.zoom = "150%";
-=======
-/*function darkmode () {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  } */
-
-document.body.style.zoom = "300%"
->>>>>>> 72e8824e34b54e7fa8c98ad0a455daf389a9b7d9
