7019a457a5675620c028e6a381c34300145025c5
mujtabanadeem <mujtaba.nadeem4@gmail.com>
2023-01-14 20:41:07 +0000

    namecheap test

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,20 +1,3 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-/*if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark: text-[#57CC99]')
-  } else {
-    document.documentElement.classList.remove('dark: text-[#57CC99]')
-  }
-  
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = 'dark'
-  
-  // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem('theme')
 
-/*function darkmode () {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  } */
+
+  document.body.style.zoom = "150%";
